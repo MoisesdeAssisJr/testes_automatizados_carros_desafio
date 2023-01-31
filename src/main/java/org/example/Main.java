cfa21b0390b001bd507d4d9307cd10fc148d102c
Moises-de-Assis <moisesdeassis.dev@gmail.com>
2023-01-30 21:59:57 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+ /** Projeto proposto como exercício na disciplina de automação de testes I;
+  * Bootcamp Santander Coders by Ada.
+  * Trilha de automação de testes.
+  */
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
